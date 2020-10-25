90f7ee2722a0decfdb24c85fdd1e6fc44d72403e
Alex Eesley <alexandra.eesley@gmail.com>
2020-10-25 13:02:30 -0500

    adding changes necessary to use Heroku

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 1600;
+const PORT = process.env.PORT || 1600;
 const fs = require('fs')
 
 // SETTING UP EXPRESS TO HANDLE DATA
