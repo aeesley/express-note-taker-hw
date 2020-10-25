76fad7ea75bcc3e76478c4d3ee95886ea66340c0
Alex Eesley <alexandra.eesley@gmail.com>
2020-10-25 11:07:17 -0500

    setting up express and adding first intial paths

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,21 @@
+// SETTING UP REQUIRES AND THE LOCAL HOST PORT
 const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = 8080;
-
 const fs = require('fs')
+
+// SETTING UP EXPRESS TO HANDLE DATA
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
+
+// PATHS
+app.get("/", function(req, res) {
+    console.log('we smack the route!!')
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+    });
